8962755887cbc5033dc047153c0178da9457b518
Oumar DIarra <Oumard7>
2024-12-17 02:47:19 -0500

    Declaration of function

---
+++ inventory.h
@@ -0,0 +1,17 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+typedef struct {
+    char **items;
+    int item_count   //stuff in the inventory 
+    int capacity; //The capacity of my inventory
+} INVENTORY
+
+
+
+
+
+
+
+
+#endif

0e35ce433261c06df9316dafe5a9df1c02902172
Oumar DIarra <Oumard7>
2024-12-19 03:38:48 -0500

    Dungeon command header file

---
+++ commandParser.h
@@ -0,0 +1,8 @@
+#ifndef COMMAND_PARSER_H
+#define COMMAND_PARSER_H
+
+#include "roomManip.h"
+#include "inventory.h"
+
+void processCommand(const char *command, ROOM **current_room, LEVEL **current_level, INVENTORY *inventory)
+#endif

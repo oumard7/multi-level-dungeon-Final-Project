66366813d2c3b16e5aacbfd39a7aedcedffe47be
Oumar DIarra <Oumard7>
2024-12-20 12:13:06 -0500

    Room check and handling

--- commandParser.c
+++ commandParser.c
@@ -11,6 +11,44 @@ void processCommand(const char *command, ROOM **current_room, LEVEL **current_le
     	char direction = command[5]; // Take direction from the command
     	ROOM *next_room = NULL;
 
-    
+    	
+    	// Determine the next room based on the direction
+    	switch (direction) {
+        	case 'n':
+            	next_room = (*current_room)->n;
+            	break;
+        	case 'e':
+            	next_room = (*current_room)->e;
+            	break;
+        	case 's':
+            	next_room = (*current_room)->s;
+            	break;
+        	case 'w':
+            	next_room = (*current_room)->w;
+            	break;   
+        	default:
+            	printf("Invalid. Use move n, move e, move s, or move w.\n");
+            	return;
+    	}
+
+    	// Check if the movement is valid
+    	if (next_room)
+ 		{
+        	*current_room = next_room; // Update the pointer
+        	printf("Moving to a new room...\n");
+
+	// Check if the room has loot
+        	if ((*current_room)->visited == 0 && (*current_room)->has_loot == 1)
+		 { 
+            	addItem(inventory, "Key");
+            	printf("You found a Key!\n");
+            	(*current_room)->has_loot = 0; // Mark loot as collected
+        	}
+
+        	// Check if the room is a portal
+        	if ((*current_room)->is_portal) {
+            	printf("Ohh. You entered the portal to the next level...\n");
+
+
 
 

616e1e7c9003b59b24cb46e5c30e68bf97c6f75d
Oumar DIarra <Oumard7>
2024-12-20 18:49:00 -0500

    Commit SAVE/LOAD STATE GAME

--- fileIO.c
+++ fileIO.c
@@ -1,13 +1,23 @@
+#Include "fileIO.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 void saveGame(LEVEL *level, INVENTORY *inventory, const char *filename) {
     FILE *file = fopen(filename, "w");
     if (!file) {
         printf("Cannot save the game.\n");
         return;
     }
-    fclose(file);
+	fclose(file);
+}
 
+void loadGame(LEVEL **level, INVENTORY **inventory, const char *filename) {
+	FILE *file = fopen(filename, "r");
+	if (!file) {
+    	printf("Can't load the game.\n");
+    	return;
+	}
 
+        fclose(file);
+
+}
 

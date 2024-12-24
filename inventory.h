3392a8c34fb2c7bc9bd51744e2a102a2b862c363
oumard7 <158207233+oumard7@users.noreply.github.com>
2024-12-23 21:57:59 -0500

    Update inventory.h

--- inventory.h
+++ inventory.h
@@ -3,13 +3,13 @@
 
 typedef struct {
     char **items;
-    int item_count   //stuff in the inventory 
+    int item_count;  //stuff in the inventory 
     int capacity; //The capacity of my inventory
-} INVENTORy
+} INVENTORY;
 
-INVENTORY *createInventory(int capacity)  // Create a new inventory
-void addItem(INVENTORY *inventory, const char *item)  // Add an item to the inventory
-void displayInventory(INVNTORY *inventory);  // Print the items in the inventory
+INVENTORY *createInventory(int capacity);  // Create a new inventory
+void addItem(INVENTORY *inventory, const char *item);  // Add an item to the inventory
+void displayInventory(INVENTORY *inventory);  // Print the items in the inventory
 void freeInventory(INVENTORY *inventory); // Then free the memory allocation
 
 
